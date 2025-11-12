c461baf4d926315d45c408db34a0b6896e5ff959
Kizaaaa <13522059@std.stei.itb.ac.id>
2025-11-12 15:53:46 +0700

    chore: blur name

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,4 +1,5 @@
 import defaultComponents from 'fumadocs-ui/mdx';
+import { ImageZoom } from 'fumadocs-ui/components/image-zoom';
 import { APIPage } from 'fumadocs-openapi/ui';
 import { openapi } from '@/lib/openapi';
 import type { MDXComponents } from 'mdx/types';
@@ -6,6 +7,7 @@ import type { MDXComponents } from 'mdx/types';
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
     ...defaultComponents,
+    img: (props) => <ImageZoom {...(props as any)} />,
     APIPage: (props) => <APIPage {...openapi.getAPIPageProps(props)} />,
     ...components,
   };
