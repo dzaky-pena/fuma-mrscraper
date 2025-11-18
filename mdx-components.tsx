8adb972a6aeab51aac6e491dfdffdf5df4471ec4
Kizaaaa <13522059@std.stei.itb.ac.id>
2025-11-18 15:45:14 +0700

    feat: listing revision

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -3,6 +3,7 @@ import { ImageZoom } from 'fumadocs-ui/components/image-zoom';
 import { APIPage } from 'fumadocs-openapi/ui';
 import { openapi } from '@/lib/openapi';
 import type { MDXComponents } from 'mdx/types';
+import * as TabsComponents from 'fumadocs-ui/components/tabs';
 
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
@@ -10,5 +11,6 @@ export function getMDXComponents(components?: MDXComponents): MDXComponents {
     img: (props) => <ImageZoom {...(props as any)} />,
     APIPage: (props) => <APIPage {...openapi.getAPIPageProps(props)} />,
     ...components,
+    ...TabsComponents,
   };
 }
