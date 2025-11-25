8459ed965d323a79374d557a8c4fdeb378237942
Kizaaaa <13522059@std.stei.itb.ac.id>
2025-11-25 09:58:07 +0700

    feat: navbar on homepage

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -1,6 +1,18 @@
-import { HomeLayout } from 'fumadocs-ui/layouts/home';
+import { source } from '@/lib/source';
+import { DocsLayout } from 'fumadocs-ui/layouts/notebook';
 import { baseOptions } from '@/lib/layout.shared';
 
-export default function Layout({ children }: LayoutProps<'/'>) {
-  return <HomeLayout {...baseOptions()}>{children}</HomeLayout>;
+export default function Layout({ children }: LayoutProps<'/docs'>) {
+  const { nav, ...base } = baseOptions();
+  return (
+    <div id="landing-page">
+      <DocsLayout
+        tree={source.pageTree}
+        {...base}
+        nav={{ ...nav, mode: 'top' }}
+      >
+        {children}
+      </DocsLayout>
+    </div>
+  );
 }
