5f0e89f7199a822cdcc8d561dacec7e1ccb7896c
Kizaaaa <13522059@std.stei.itb.ac.id>
2025-11-25 12:14:14 +0700

    feat: Hero Section

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,10 +1,12 @@
 import { FeaturesSection } from './components/features/features.component';
 import { GetStartedSection } from './components/get-started/get-started.component';
+import { HeroSection } from './components/hero-section/hero-section';
 import { IntegrationsSection } from './components/integrations/integrations.component';
 
 export default function HomePage() {
   return (
     <div className="flex flex-col justify-center text-center">
+      <HeroSection/>
       <GetStartedSection/>
       <FeaturesSection/>
       <IntegrationsSection/>
